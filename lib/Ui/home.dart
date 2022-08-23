75b8e090df1e633c32876494eaf4627707397432
20191737seif <tsaif031@gmail.com>
2022-08-23 21:38:29 +0200

    cards
    I made a cards function to make it easy in the main to call many cards and give them photos and title

--- lib/Ui/home.dart
+++ lib/Ui/home.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:movie_app/cards.dart';
 import '../models/movie.dart';
 import '../onbording.dart';
 
@@ -14,7 +14,23 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Onboarding(number: 3,images: data,titles: data,content: data,txtColor: Colors.white,),
-    );
+        body: SizedBox( height: MediaQuery.of(context).size.height,
+          child: Column(
+      children: [
+          Container(
+            width: 200,
+            height: 200,
+            child: cards(
+                images:
+                    "https://images.unsplash.com/photo-1607603750909-408e193868c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+                titles: "hello"),
+          ),
+          cards(
+              images:
+                  "https://images.unsplash.com/photo-1607603750909-408e193868c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+              titles: "hello"),
+      ],
+    ),
+        ));
   }
 }
---
+++ lib/cards.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/models/movie.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+
+class cards extends StatefulWidget {
+  String images;
+  String titles;
+
+  cards({
+    Key? key,
+
+    required this.images,
+    required this.titles,
+
+  }) : super(key: key);
+
+  @override
+  State<cards> createState() => _cards();
+}
+
+class _cards extends State<cards> {
+  final controller = PageController();
+  @override
+  Widget build(BuildContext context) {
+    return (
+       Container(
+        width: 200,
+        height: 200,
+
+        child:   Column(
+          children: [
+            Image.network(widget.images),
+            Text(widget.titles)
+          ],
+        ),
+         margin: EdgeInsets.all(10),
+        decoration: BoxDecoration( color: Colors.white,
+          borderRadius: BorderRadius.circular(5.0)
+              ,boxShadow: [BoxShadow(blurRadius: 0.2,offset: Offset(5,5),color: Colors.grey.shade800)]
+
+
+
+        ),
+      )
+    );
+  }
+}
